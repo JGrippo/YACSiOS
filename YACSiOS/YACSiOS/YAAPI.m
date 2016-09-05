ee42516bcf9620c7e26adb681880604a8ada2393
James Grippo <grippj@rpi.edu>
2016-09-04 21:30:19 -0400

    Cleaned up code and commented the YAAPI.m.

--- YACSiOS/YACSiOS/YAAPI.m
+++ YACSiOS/YACSiOS/YAAPI.m
@@ -10,6 +10,7 @@
 
 @implementation YAAPI : AFHTTPSessionManager
 
+// This method creates an API object that communicates with the api with different requests. Its how we access the other functions for this class.
 +(YAAPI*) API
 {
     static YAAPI* netManager = nil;
@@ -23,6 +24,8 @@
     return netManager;
 }
 
+// This is the API request for getting the departments, schools, etc for the catalog view.
+// The format of the resonse object should be a dictionary with the Keys in this order : "schools" -> "departments" -> other attributes such as "code"/"name"/"id"
 -(void) getDepartments:(void (^)(NSDictionary *))completion
 {
     [self GET:@"/api/v5/schools.json?&show_departments" parameters:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nonnull responseObject) {
@@ -32,6 +35,8 @@
         completion(nil);
     }];
 }
+
+// This is the API request to get a dictionary of each section in the format : "courses" -> "sections" -> other attributes "seats" / "seats taken" / "instructor"
 -(void) getCoursesForDepartment:(NSNumber*) department_id completion:(void (^)(NSDictionary *))completion
 {
     NSString* url = [NSString stringWithFormat:@"/api/v5/courses.json?department_id=%@&show_sections", department_id];
